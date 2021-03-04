892234988b6615a8011ad5e7cbe6316c4210de1b
Abdullah Minawal <abdullah.minawal@hotmail.de>
2021-03-04 14:18:42 +0100

    routes

--- server.js
+++ server.js
@@ -1,60 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const Card = require('./models/Card')
-const User = require('./models/User')
+const setupMongo = require('./setupMongo')
 
-mongoose
-  .connect('mongodb://localhost/lean-coffee-board', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to mongodb'))
-  .catch(error => console.error('Could not connect to mongodb', error))
-
+setupMongo()
 const app = express()
 
 app.use(express.json()) // add middleware for json data
-
-app.get('/api/users', async (req, res, next) => {
-  res.json(await User.find().catch(next))
-})
-
-app.get('/api/users/:id', async (req, res, next) => {
-  const { id } = req.params
-  res.json(await User.findOne({ id }).catch(next))
-})
-
-app.delete('/api/users/:id', async (req, res, next) => {
-  const { id } = req.params
-  res.json(await User.deleteOne({ id }).catch(next))
-})
-
-app.post('/api/users', async (req, res, next) => {
-  res.json(await User.create(req.body).catch(next))
-})
-
-app.get('/api/cards', async (req, res, next) => {
-  res.json(await Card.find().populate('author').catch(next))
-})
-
-app.get('/api/cards/:id', async (req, res, next) => {
-  const { id } = req.params
-  res.json(await Card.findOne({ id }).populate('author').catch(next))
-})
-
-app.delete('/api/cards/:id', async (req, res, next) => {
-  const { id } = req.params
-  res.json(await Card.deleteOne({ id }).catch(next))
-})
-
-app.post('/api/cards', async (req, res, next) => {
-  res.json(await Card.create(req.body).catch(next))
-})
-
-app.use((err, req, res, next) => {
-    console.log(err.message)
-    res.json({ error: err })
-})
+app.use('/api/users', require('./routes/users'))
+app.use('/api/cards', require('./routes/cards'))
+app.use(require('./routes/error'))
 
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000')
