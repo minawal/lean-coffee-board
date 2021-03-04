eadf94a6108b01f759d18af67cc7ebf550b14c64
Abdullah Minawal <abdullah.minawal@hotmail.de>
2021-03-04 11:14:53 +0100

    Cards

--- server.js
+++ server.js
@@ -1,61 +1,54 @@
 const express = require('express')
-const {v4} = require('uuid')
 const mongoose = require('mongoose')
-const User = require('./User')
+const Card = require('./models/Card')
+const User = require('./models/User')
 
 mongoose
-.connect('mongodb://localhost/lean-coffee-board', {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-})
-
-.then(() => console.log('Connected to mongodb'))
-.catch(error => console.log('Could not connect to mongodb', error))
+  .connect('mongodb://localhost/lean-coffee-board', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to mongodb'))
+  .catch(error => console.error('Could not connect to mongodb', error))
 
 const app = express()
 
-let users = []
-
-
 app.use(express.json()) // add middleware for json data
 
-/*
-req -> express -> middleware -> middleware -> middleware
-middleware = (req, res, next) => { }
-app.use((req, res, next) => {
-  if (req.method === 'GET' && req.url === '/api/users') {
-    res.json(users)
-  } else {
-    next()
-  }
-})
-*/
-
 app.get('/api/users', async (req, res) => {
-  // User.find().then(users => res.json(users))
   res.json(await User.find())
 })
 
 app.get('/api/users/:id', async (req, res) => {
   const { id } = req.params
-  res.json(await User.findOne({id}))
+  res.json(await User.findOne({ id }))
 })
 
-app.delete('/api/users/:id', (req, res) => {
+app.delete('/api/users/:id', async (req, res) => {
   const { id } = req.params
-  users = users.filter(user => user.id !== id)
-  res.json(users)
+  res.json(await User.deleteOne({ id }))
 })
 
 app.post('/api/users', async (req, res) => {
-  // exercise: add id (via uuid) for each new user
-  // const newUser = { ...req.body, id: v4() }
-  // users.push(newUser)
   res.json(await User.create(req.body))
 })
 
-app.get('/api/cards', (req, res) => {
-  res.json([{ title: 'First card' }])
+app.get('/api/cards', async (req, res) => {
+  res.json(await Card.find().populate('author'))
+})
+
+app.get('/api/cards/:id', async (req, res) => {
+  const { id } = req.params
+  res.json(await Card.findOne({ id }).populate('author'))
+})
+
+app.delete('/api/cards/:id', async (req, res) => {
+  const { id } = req.params
+  res.json(await Card.deleteOne({ id }))
+})
+
+app.post('/api/cards', async (req, res) => {
+  res.json(await Card.create(req.body))
 })
 
 app.listen(3000, () => {
